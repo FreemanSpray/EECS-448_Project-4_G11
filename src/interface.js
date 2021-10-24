46cd9c84c24dc55ea9d136f97c49f7ada0095010
FreemanSpray <89803643+FreemanSpray@users.noreply.github.com>
2021-10-24 15:11:35 -0500

    Created files with interface/draw functionality

---
+++ src/interface.js
@@ -0,0 +1,7 @@
+let canvas
+let context
+
+document.addEventListener("DOMContentLoaded", () => {
+	canvas = document.querySelector("#myCanvas")
+	context = canvas.getContext("2d")
+})
---
+++ src/main.js
@@ -0,0 +1,11 @@
+function initialize(){
+
+}
+
+document.addEventListener("click", e => {
+    console.log([e.x, e.y])
+})
+
+document.addEventListener("keypress", f => {
+    console.log(f)
+})
