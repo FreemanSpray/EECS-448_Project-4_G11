f7e684c437bc4056ea00c3304b2ec8bb3e5bb9e5
jakeangus <60803490+jakeangus@users.noreply.github.com>
2021-10-26 16:29:44 -0500

    basics of UI implementation *needs work*

--- src/interface.js
+++ src/interface.js
@@ -2,34 +2,64 @@ let canvas
 let context
 
 document.addEventListener("DOMContentLoaded", () => {
-	canvas = document.querySelector("#myCanvas")
-	context = canvas.getContext("2d")
-})
+    drawTemplate();
+});
 
-function drawTemplate(){
+function drawTemplate() {
+
+    // draw board
+    canvas = document.getElementById("board");
+    ctx = canvas.getContext("2d");
+    //loop for drawing grid lines
+    for (var i = 0; i <= 1400; i = i + 40) {
+
+        ctx.beginPath();
+        //vertical lines
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, 1400);
+
+        //horizontal lines
+        ctx.moveTo(0, i);
+        ctx.lineTo(1400, i);
+
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
+
+    //draw cards
+    canvas = document.getElementById("cards");
+    ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.lineWidth = "3";
+    ctx.strokeStyle = "black";
+
+    //loop draws card rectangles but very messily
+    for (var i = 80; i <= 704; i = i + 156) {
+        ctx.rect(i, 20, 100, 150);
+        ctx.stroke();
+    }
+}
+
+function drawVisibleUnits() {
 
 }
 
-function drawVisibleUnits(){
-	
-}
-
-function drawInvisibleUnits(){
-	
-}
-
-function drawHand(){
-	
-}
-
-function drawSelectedInfo(){
-	
-}
-
-function highlightTiles_MoveRange(){
+function drawInvisibleUnits() {
 
 }
 
-function highlightTiles_ActionRange(){
-	
+function drawHand() {
+
+}
+
+function drawSelectedInfo() {
+
+}
+
+function highlightTiles_MoveRange() {
+
+}
+
+function highlightTiles_ActionRange() {
+
 }
