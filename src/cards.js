d2139cf0bdc1120fc20d05d38a1dcf10833eb874
LiquidEnder <79119607+LiquidEnder@users.noreply.github.com>
2021-11-18 18:24:50 -0600

    made unit descriptions look pretty

--- src/cards.js
+++ src/cards.js
@@ -34,33 +34,32 @@ function initializeCard(card){
     }
 
     else if (name == "Knight") {
-        card.text = `Summon range: 3 tiles\n
-
-        Move: Knight moves\n to target unoccupied\n tile within 3 tiles\n vertical or horizontal.\n
-    
-        Charge: Destroy target\n unit within 6 tiles\n vertical or horizontal\n of Knight. If the\n unit is destroyed this\n way, Knight moves\n to that tile.`
+        card.text = `Charge: Destroy target\n unit within 6 tiles\n vertical or horizontal\n of Knight. If the\n unit is destroyed this\n way, Knight moves\n to that tile.`
         card.ability = new ActivatedAbility("Knight: Summon", card.owner.commander)
     }
     
     else if (name == "Guard Tower") {
-        card.text = `Summon range: 4 tiles\n
-        
+        card.text = `Stationary: Cannot move\n
         Watchful Eye:\n When a unit an\n opponent controls\n moves to within 2\n tiles of Guard Tower,\n destroy that unit.`
         card.ability = new ActivatedAbility("Guard Tower: Summon", card.owner.commander)
     }
     
     else if (name == "Fireball") {
-        card.text = ``
+        card.text = `KaBoom:\n Destroys everything\n within 3 tiles.`
         card.ability = new ActivatedAbility("Fireball", card.owner.commander)
     }
 
     else if (name == "Assassin") {
-        card.text = ``
+        card.text = `Assassinate: Assassin\n destroys target unit\n within 1 tile vertical\n or horizontal.`
         card.ability = new ActivatedAbility("Assassin: Summon", card.owner.commander)
     }
 
     else if (name == "Firebound Apprentice") {
-        card.text = ``
+        card.text = `Burn Bright:\n Begin with 5\n charge counters. When\n no charge counters remain,\n destroy this unit.
+
+        (1)Explosive Entrance:\nUnit moves to tile\n Destroy each unit within\n 1 tile of unit.
+    
+        (2)Firebolt:\n Unit destroys enemy`
         card.ability = new ActivatedAbility("Firebound Apprentice: Summon", card.owner.commander)
     }
 }
